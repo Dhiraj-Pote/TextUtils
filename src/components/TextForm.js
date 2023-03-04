8b9cd3c9b56e3d91e259db8ba4d17bddebcc741c
Dhiraj-Pote <dhirajpote29@gmail.com>
2023-03-04 11:29:27 +0530

    changes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -38,9 +38,9 @@ export default function TextForm(props) {
         <div className="mb-3">
           <h1><label htmlFor="Box" className="form-label">{props.text}</label></h1>
           <textarea className="form-control" value={text} onChange={handleOnChange} id="Box" rows="7"></textarea>
-          <button className="btn btn-primary mx-3 my-3" onClick={handleOnUpClick}>Convert to Upper</button>
-          <button className="btn btn-primary mx-3" onClick={handleOnLoClick}>Convert to Lower</button>
-          <button className="btn btn-info mx-3"  onClick={handleOnExClick}>Solves Expression</button>
+          <button className="btn btn-primary mx-1 my-2" onClick={handleOnUpClick}>Convert to Upper</button>
+          <button className="btn btn-primary mx-1 my-2" onClick={handleOnLoClick}>Convert to Lower</button>
+          <button className="btn btn-info mx-1 my-2"  onClick={handleOnExClick}>Solves Expression</button>
         </div>
         <div className="container">
           <h2>Your Text Summary</h2>
